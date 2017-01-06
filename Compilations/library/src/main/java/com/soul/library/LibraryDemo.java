db6cd53abd7ecaadc0aaf8d8381974ca2e5d6f26
soul <405557524@qq.com>
2017-01-06 14:43:34 +0800

    添加library模块

---
+++ Compilations/library/src/main/java/com/soul/library/LibraryDemo.java
@@ -0,0 +1,18 @@
+package com.soul.library;
+
+/**
+ * * @author soul
+ *
+ * @项目名:Compilations
+ * @包名: com.soul.library
+ * @作者：祝明
+ * @描述：TODO
+ * @创建时间：2017/1/6 14:43
+ */
+
+public class LibraryDemo {
+
+    public static  String getString(){
+        return "hello";
+    }
+}

8b3fbef479f492203d22b0a79b1fde348f10672e
soul <405557524@qq.com>
2017-02-07 19:26:36 +0800

    添加service ,broadcast demo

---
+++ Compilations/library/src/main/java/com/soul/library/mvp/IPresenter.java
@@ -0,0 +1,26 @@
+package com.soul.library.mvp;
+
+/**
+ * * @author soul
+ *
+ */
+
+public interface IPresenter<V extends IView> {
+
+    /**
+     * @param view 绑定
+     */
+    void attachView(V view);
+
+
+    /**
+     * 防止内存的泄漏,清楚presenter与activity之间的绑定
+     */
+    void detachView();
+
+
+    /**
+     * @return 获取View
+     */
+    IView getIView();
+}
